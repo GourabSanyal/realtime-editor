ff4763fa9331fd13b9bc0b3778d77d02fa421f5c
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-02-10 17:13:35 +0530

    code mirror added

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { EditorView, basicSetup } from "codemirror";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 const Editor = () => {
-  return <div>Editor Goes here</div>;
+  useEffect(() => {
+    async function init() {
+      // EditorView.fromTextArea(document.getElementById("realtimeEditor"), {
+      //   mode: { name: "javascript", JSON: true },
+      // });
+      <CodeMirror
+        value="console.log('hello world!');"
+        height="100vh"
+        theme="dark"
+        extensions={[javascript({ jsx: true })]}
+        // onChange={onChange}
+      />;
+    }
+    init();
+  }, []);
+
+  return (
+    <div>
+      <CodeMirror
+        value="console.log('hello world!');"
+        theme="dark"
+        extensions={[javascript({ jsx: true }, { JSON: true })]}
+        // onChange={onChange}
+      />
+      ;
+    </div>
+  );
 };
 
 export default Editor;
