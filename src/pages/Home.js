7a133dc9399c0ea1fa965a72a1bdb83586e88c70
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-02-10 12:40:40 +0530

    home screen input handlers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    // console.log(id);
+  };
+
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
@@ -11,12 +22,24 @@ const Home = () => {
         />
         <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
         <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder="ROOM ID" />
-          <input type="text" className="inputBox" placeholder="USER NAME" />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USER NAME"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+          />
           <button className="btn joinBtn">Join</button>
           <span className="createInfo">
             If you don't have an invite create &nbsp;{" "}
-            <a href="#" className="createNewBtn">
+            <a onClick={createNewRoom} href="#" className="createNewBtn">
               new room
             </a>
           </span>
